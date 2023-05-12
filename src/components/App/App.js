d81ae1b62b17a8cc4fbd4bf8cc443019826d3c91
Valerii Pometun <vmpometun@gmail.com>
2023-05-12 12:23:10 +0300

    start refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { PixabayAPI } from 'services/pixabay-api';
 import { Container } from './styled';
 import Searchbar from 'components/Searchbar';
 import ImageGallery from 'components/ImageGallery';
 import Button from 'components/Button';
 import Loader from 'components/Loader';
+
+const pixabayApi = new PixabayAPI();
 
 class App extends Component {
   state = {
@@ -13,43 +16,50 @@ class App extends Component {
     visible: false,
   };
 
-  handleSubmitForm = (data, query, page) => {
-    if (this.state.query === query || this.state.pageNumber === 1) {
-      try {
-        this.setState(prevState => ({
-          cards: [...prevState.cards, ...data],
-          query: query,
-          pageNumber: page,
-        }));
-      } catch (error) {}
-    }
-    if (this.state.query !== query) {
-      try {
-        this.setState(prevState => ({
-          cards: data,
-          query: query,
-          pageNumber: 1,
-        }));
-      } catch (error) {}
+  // handleSubmitForm = (data, query, page) => {
+  //   if (this.state.query === query || this.state.pageNumber === 1) {
+  //     try {
+  //       this.setState(prevState => ({
+  //         cards: [...prevState.cards, ...data],
+  //         query: query,
+  //         pageNumber: page,
+  //       }));
+  //     } catch (error) {}
+  //   }
+  //   if (this.state.query !== query) {
+  //     try {
+  //       this.setState(prevState => ({
+  //         cards: data,
+  //         query: query,
+  //         pageNumber: 1,
+  //       }));
+  //     } catch (error) {}
+  //   }
+  // };
+
+  handleSubmitForm = async query => {
+    try {
+      const { data } = await pixabayApi.fetchPhotos(
+        query,
+        this.state.pageNumber
+      );
+      this.setState(prevState => ({
+        cards: [...prevState.cards, ...data.hits],
+        query: query,
+        pageNumber: prevState.pageNumber + 1,
+      }));
+    } catch (error) {
+      console.log(error);
     }
   };
 
   render() {
     return (
       <Container>
-        <Searchbar
-          onSubmit={this.handleSubmitForm}
-          page={this.state.pageNumber}
-        />
+        <Searchbar handleSubmit={this.handleSubmitForm} />
         {this.state.visible && <Loader />}
         <ImageGallery cards={this.state.cards}></ImageGallery>
-        {this.state.query && (
-          <Button
-            onSubmit={this.handleSubmitForm}
-            page={this.state.pageNumber}
-            query={this.state.query}
-          />
-        )}
+        <Button handleSubmit={this.handleSubmitForm} query={this.state.query} />
       </Container>
     );
   }
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -2,41 +2,27 @@
 import React, { Component } from 'react';
 import { Header, Form, SearchFormBtn, SearchFormInput } from './styled';
 import { ReactComponent as SearchIcon } from '../../icons/search.svg';
-import { PixabayAPI } from 'services/pixabay-api';
-
-const pixabayApi = new PixabayAPI();
 
 class Searchbar extends Component {
   state = {
     query: '',
   };
 
-  handleSubmitForm = async e => {
-    e.preventDefault();
-    const newPage = this.props.page + 1;
-
-    try {
-      const { data } = await pixabayApi.fetchPhotos(
-        this.state.query,
-        this.props.page
-      );
-      this.props.onSubmit(data.hits, this.state.query, newPage);
-    } catch (error) {
-      console.log(error);
-    }
-
-    this.setState({ query: '' });
-  };
-
   addQueryName = event => {
     this.setState({ query: event.target.value });
+  };
+
+  handleSubmitForm = e => {
+    e.preventDefault();
+    this.props.handleSubmit(this.state.query);
+    this.setState({ query: '' });
   };
 
   render() {
     return (
       <Header>
         <Form onSubmit={this.handleSubmitForm}>
-          <SearchFormBtn type="submit" disabled={this.state.query === ''}>
+          <SearchFormBtn type="submit">
             <SearchIcon width="30" height="30" />
           </SearchFormBtn>
 
